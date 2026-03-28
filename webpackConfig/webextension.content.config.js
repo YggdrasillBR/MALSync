0d60fea1d483d67eb00b5033be884bf45d4ceaca
YggdrasillBR <96698392+YggdrasillBR@users.noreply.github.com>
2026-03-28 16:18:43 -0300

    some conversions

--- webpackConfig/webextension.content.config.js
+++ webpackConfig/webextension.content.config.js
@@ -1,67 +1,67 @@
-const webpack = require('webpack');
-const path = require('path');
-const fs = require('fs');
-const { VueLoaderPlugin } = require('vue-loader');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+import { ProvidePlugin, DefinePlugin } from 'webpack';
+import { join, resolve as _resolve } from 'path';
+import { existsSync } from 'fs';
+import { VueLoaderPlugin } from 'vue-loader';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 const pages = require('./utils/pages').pages();
-const { getKeys } = require('./utils/keys');
-const { getVirtualScript } = require('./utils/general');
+import { getKeys } from './utils/keys';
+import { getVirtualScript } from './utils/general';
 
 let entry = {
-  'content-script': path.join(
+  'content-script': join(
     __dirname,
     '..',
     'src/index-webextension/content.ts',
   ),
-  'mal-script': path.join(
+  'mal-script': join(
     __dirname,
     '..',
     'src/index-webextension/myanimelist.ts',
   ),
-  'anilist-script': path.join(
+  'anilist-script': join(
     __dirname,
     '..',
     'src/index-webextension/anilist.ts',
   ),
-  'kitsu-script': path.join(
+  'kitsu-script': join(
     __dirname,
     '..',
     'src/index-webextension/kitsu.ts',
   ),
-  'simkl-script': path.join(
+  'simkl-script': join(
     __dirname,
     '..',
     'src/index-webextension/simkl.ts',
   ),
-  'oauth-script': path.join(
+  'oauth-script': join(
     __dirname,
     '..',
     'src/index-webextension/oauth.ts',
   ),
-  'oauth-anilist-script': path.join(
+  'oauth-anilist-script': join(
     __dirname,
     '..',
     'src/index-webextension/anilistOauth.ts',
   ),
-  'oauth-shiki-script': path.join(
+  'oauth-shiki-script': join(
     __dirname,
     '..',
     'src/index-webextension/shikiOauth.ts',
   ),
-  'pwa-script': path.join(
+  'pwa-script': join(
     __dirname,
     '..',
     'src/index-webextension/pwa.ts',
   ),
-  iframe: path.join(__dirname, '..', 'src/iframe.ts'),
-  popup: path.join(__dirname, '..', 'src/popup.ts'),
+  iframe: join(__dirname, '..', 'src/iframe.ts'),
+  popup: join(__dirname, '..', 'src/popup.ts'),
 }
 
 pages.forEach(page => {
-  pageRoot = path.join(__dirname, '..', 'src/pages/', page);
-  entry['page_' + page] = 'expose-loader?exposes=_Page|' + page + '!' + path.join(pageRoot, 'main.ts');
-  if (fs.existsSync(path.join(pageRoot, 'proxy.ts'))) {
+  pageRoot = join(__dirname, '..', 'src/pages/', page);
+  entry['page_' + page] = 'expose-loader?exposes=_Page|' + page + '!' + join(pageRoot, 'main.ts');
+  if (existsSync(join(pageRoot, 'proxy.ts'))) {
     entry['proxy/proxy_' + page] = getVirtualScript('proxy_' + page, `
       import { script } from './src/pages/${page}/proxy.ts';
       import { ScriptProxyWrapper } from './src/utils/scriptProxyWrapper.ts';
@@ -73,70 +73,68 @@ pages.forEach(page => {
 
 console.log(entry);
 
-module.exports = {
-  entry: entry,
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-        options: {
-          appendTsSuffixTo: [/\.vue$/],
-        },
+export const entry = entry;
+export const module = {
+  rules: [
+    {
+      test: /\.tsx?$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/,
+      options: {
+        appendTsSuffixTo: [/\.vue$/],
       },
-      {
-        test: /\.less$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'less-loader'],
-      },
-      {
-        test: /\.vue$/,
-        exclude: /node_modules/,
-        loader: 'vue-loader',
-        options: {
-          customElement: true,
-          shadowMode: true,
-          exposeFilename: true,
-        },
-      },
-    ],
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.less', '.vue'],
-    alias: {
-      vue: '@vue/runtime-dom',
     },
-  },
-  mode: 'development',
-  output: {
-    filename: 'content/[name].js',
-    path: path.resolve(__dirname, '..', 'dist', 'webextension'),
-  },
-  plugins: [
-    new ForkTsCheckerWebpackPlugin({
-      typescript: {
-        configFile: path.resolve(__dirname, '../tsconfig.json'),
-        extensions: {
-          vue: {
-            enabled: true,
-            compiler: '@vue/compiler-sfc',
-          },
-        },
+    {
+      test: /\.less$/,
+      exclude: /node_modules/,
+      use: ['style-loader', 'css-loader', 'less-loader'],
+    },
+    {
+      test: /\.vue$/,
+      exclude: /node_modules/,
+      loader: 'vue-loader',
+      options: {
+        customElement: true,
+        shadowMode: true,
+        exposeFilename: true,
       },
-    }),
-    new VueLoaderPlugin(),
-    new webpack.ProvidePlugin({
-      con: path.resolve(__dirname, './../src/utils/console'),
-      utils: path.resolve(__dirname, './../src/utils/general'),
-      j: path.resolve(__dirname, './../src/utils/j'),
-      api: path.resolve(__dirname, './../src/api/webextension'),
-    }),
-    new webpack.DefinePlugin({
-      __VUE_OPTIONS_API__: true,
-      __VUE_PROD_DEVTOOLS__: false,
-      __MAL_SYNC_KEYS__: JSON.stringify(getKeys()),
-    }),
+    },
   ],
 };
+export const devtool = 'source-map';
+export const resolve = {
+  extensions: ['.tsx', '.ts', '.js', '.less', '.vue'],
+  alias: {
+    vue: '@vue/runtime-dom',
+  },
+};
+export const mode = 'development';
+export const output = {
+  filename: 'content/[name].js',
+  path: _resolve(__dirname, '..', 'dist', 'webextension'),
+};
+export const plugins = [
+  new ForkTsCheckerWebpackPlugin({
+    typescript: {
+      configFile: _resolve(__dirname, '../tsconfig.json'),
+      extensions: {
+        vue: {
+          enabled: true,
+          compiler: '@vue/compiler-sfc',
+        },
+      },
+    },
+  }),
+  new VueLoaderPlugin(),
+  new ProvidePlugin({
+    con: _resolve(__dirname, './../src/utils/console'),
+    utils: _resolve(__dirname, './../src/utils/general'),
+    j: _resolve(__dirname, './../src/utils/j'),
+    api: _resolve(__dirname, './../src/api/webextension'),
+  }),
+  new DefinePlugin({
+    __VUE_OPTIONS_API__: true,
+    __VUE_PROD_DEVTOOLS__: false,
+    __MAL_SYNC_KEYS__: JSON.stringify(getKeys()),
+  }),
+];
